073069f4fa29a9eb65d23de312c113ffac20a3ad
andre-carbajal <carbajalvargasandre@gmail.com>
2024-02-04 15:08:16 -0500

    change to main

--- src/main/java/net/anvian/minecontrolfx/App.java
+++ src/main/java/net/anvian/minecontrolfx/App.java
@@ -3,9 +3,11 @@ package net.anvian.minecontrolfx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class App extends Application {
     @Override
@@ -13,6 +15,7 @@ public class App extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 854, 480);
         stage.setTitle("MineControl " + Constants.VERSION);
+        stage.getIcons().add(new Image(Objects.requireNonNull(App.class.getResourceAsStream("/icon.png"))));
         stage.setScene(scene);
         stage.show();
     }
