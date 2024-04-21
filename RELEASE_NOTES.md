928fce819df1042ad2fd2a294fde096355196f28
andre-carbajal <carbajalvargasandre@gmail.com>
2024-04-20 21:55:35 -0500

    version was updated

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,4 +1,4 @@
 # Release Notes - Version 0.3
 
 ## Enhancements
-- An improvement was added to the release descriptions
+- user.dir to user.home in Mac OS
--- src/main/java/net/anvian/minecontrolfx/Constants.java
+++ src/main/java/net/anvian/minecontrolfx/Constants.java
@@ -1,5 +1,5 @@
 package net.anvian.minecontrolfx;
 
 public class Constants {
-    public static final String VERSION = "0.5";
+    public static final String VERSION = "0.6";
 }
